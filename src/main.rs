2b1fc7bbd2277ee22395a29cd8ad2ebc6c251656
Falk-Witte <krixcrox@duck.com>
2023-02-03 15:18:11 +0100

    add colored output, error handling does not work correctly

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // runame [current filename] [new filename]
 use clap::Parser;
 use std::fs::rename;
+use colored::Colorize;
 
 /// Simple file renamer
 #[derive(Parser)]
@@ -12,12 +13,20 @@ struct Args{
     current_filename: String,
     ///new filename
     new_filename: String,
+
+    /// verbose output
+    #[arg(short='v', long)]
+    verbose:bool,
     
 }
 
 fn main() {
     let args = Args::parse();
-    rename(args.current_filename, args.new_filename).unwrap_or_else(|e| println!("Error: {}", e));
+    rename(&args.current_filename, &args.new_filename).unwrap_or_else(|e| println!("Error: {}", e));
+
+    if args.verbose{
+        println!("Renamed {} to {}", args.current_filename.bold().blue(), args.new_filename.bold().green());
+    }
 }
 
 
