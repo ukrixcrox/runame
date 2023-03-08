90c00a28a57cd264a860923d728f0d7cd214ac33
Falk-Witte <krixcrox@duck.com>
2023-03-08 17:05:39 +0100

    add interactive mode

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@
 use clap::Parser;
 use std::fs::rename;
 use colored::Colorize;
+use std::io::{self, Write};
 
 /// Simple file renamer
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args{
+
     ///current filename
     current_filename: String,
+
     ///new filename
     new_filename: String,
+
+    /// interactive
+    #[arg(short='i', long)]
+    interactive:bool,
 
     /// verbose output
     #[arg(short='v', long)]
@@ -22,11 +29,42 @@ struct Args{
 
 fn main() {
     let args = Args::parse();
-    rename(&args.current_filename, &args.new_filename).unwrap_or_else(|e| println!("Error: {}", e));
 
-    if args.verbose{
-        println!("Renamed {} to {}", args.current_filename.bold().blue(), args.new_filename.bold().green());
+    if args.interactive{
+        print!("Rename {} to {}? [y/n]> ",args.current_filename.bold().blue(), args.new_filename.bold().green());
+        io::stdout().flush().unwrap_or_else(|e| eprintln!("Error: {}", e));
+
+        let mut input_buffer = String::new();
+        io::stdin().read_line(&mut input_buffer).unwrap();
+
+        match input_buffer.as_str().trim() {
+
+            "y" => {
+                rename(&args.current_filename, &args.new_filename).unwrap_or_else(|e| println!("Error: {}", e));
+                println!("Renamed {} to {}", args.current_filename.bold().blue(), args.new_filename.bold().blue());
+                std::process::exit(0x0100);
+            },
+
+            "n" => {
+                println!("{}", "Stopping operation".bold().blue());
+                std::process::exit(0x0100);
+            },
+
+            _ => {
+                println!("{}", "no input has been given!".bold().red());
+                std::process::exit(0x0100);
+            },
+
+        }
+
     }
+
+    if args.verbose && !args.interactive{
+        println!("Renamed {} to {}", args.current_filename.bold().blue(), args.new_filename.bold().green());
+        std::process::exit(0x0100);
+    }
+
+    rename(&args.current_filename, &args.new_filename).unwrap_or_else(|e| println!("Error: {}", e));
 }
 
 
